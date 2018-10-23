c1fe46ee36d5b08cd37da524302db9556f01fc48
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2018-10-23 17:08:12 +0200

    Add Don't Starve Project

---
+++ Don't Starve Camera/Assets/Don't Starve/Scripts/LookToCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookToCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotationVector = transform.rotation.eulerAngles;
+        rotationVector.y = Camera.main.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(rotationVector);
+    }
+}
